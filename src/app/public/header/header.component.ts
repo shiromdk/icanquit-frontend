c2ac5cadd73d46eefb655ca6c54e245c3aabd7ce
Alan Nguyen <shiroyasha@live.com.au>
2016-12-01 18:13:37 +1100

    added about links to xs menu

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {Routes, RouterModule, Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,20 @@ import {Component, OnInit, Input} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   public isCollapsed:boolean = true;
+  public location='';
   @Input() public set collapse(value:boolean){}
 
-  constructor() { }
+  constructor(private  _router : Router) {
+    this.location = _router.url;
+  }
 
   ngOnInit() {
   }
-
+  isAbout(){
+    this.location=this._router.url;
+    if(this.location=='/about'){
+      return true;
+    }
+    return false;
+  }
 }
